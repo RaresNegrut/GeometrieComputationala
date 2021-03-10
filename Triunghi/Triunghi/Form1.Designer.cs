1c43ad04cd47ffbf4f022a9ca0ad65a1e89757c6
TheJanitor1151 <45803183+TheJanitor1151@users.noreply.github.com>
2021-03-10 10:09:22 +0200

    Created solution for drawing triangle and medians

---
+++ Triunghi/Triunghi/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Triunghi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.Draw = new System.Windows.Forms.Button();
+            this.Medians = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(588, 224);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(633, 224);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(37, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(633, 250);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(37, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(588, 250);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(39, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(633, 276);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(37, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(588, 276);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(39, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // Draw
+            // 
+            this.Draw.Location = new System.Drawing.Point(588, 321);
+            this.Draw.Name = "Draw";
+            this.Draw.Size = new System.Drawing.Size(75, 23);
+            this.Draw.TabIndex = 7;
+            this.Draw.Text = "DRAW!";
+            this.Draw.UseVisualStyleBackColor = true;
+            this.Draw.Click += new System.EventHandler(this.Draw_Click);
+            // 
+            // Medians
+            // 
+            this.Medians.Location = new System.Drawing.Point(588, 350);
+            this.Medians.Name = "Medians";
+            this.Medians.Size = new System.Drawing.Size(129, 23);
+            this.Medians.TabIndex = 8;
+            this.Medians.Text = "DRAW MEDIANS";
+            this.Medians.UseVisualStyleBackColor = true;
+            this.Medians.Click += new System.EventHandler(this.Medians_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(588, 379);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 9;
+            this.Clear.Text = "CLEAR";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Medians);
+            this.Controls.Add(this.Draw);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button Draw;
+        private System.Windows.Forms.Button Medians;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ Triunghi/Triunghi/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triunghi
+{
+    public partial class Form1 : Form
+    {
+        static Graphics gfx;
+        static Pen pen = new Pen(Color.Black);
+        static PointF[] points;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw_Click(object sender, EventArgs e)
+        {
+
+            points = new PointF[]
+            {
+                new PointF(float.Parse(textBox1.Text),float.Parse(textBox2.Text)),
+                new PointF(float.Parse(textBox4.Text),float.Parse(textBox3.Text)),
+                new PointF(float.Parse(textBox6.Text),float.Parse(textBox5.Text)),
+            };
+
+            gfx.Clear(Color.White);
+            gfx.DrawPolygon(pen, points);
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gfx = pictureBox1.CreateGraphics();
+        }
+
+        private void Medians_Click(object sender, EventArgs e)
+        {
+            PointF midline1 = new PointF(((points[1].X + points[2].X) / 2), ((points[1].Y + points[2].Y) / 2));
+            gfx.DrawLine(pen, points[0], midline1);
+            PointF midline2 = new PointF(((points[0].X + points[2].X) / 2), ((points[0].Y + points[2].Y) / 2));
+            gfx.DrawLine(pen, points[1], midline2);
+            PointF midline3 = new PointF(((points[0].X + points[1].X) / 2), ((points[0].Y + points[1].Y) / 2));
+            gfx.DrawLine(pen, points[2], midline3);
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            gfx.Clear(Color.White);
+        }
+    }
+}
